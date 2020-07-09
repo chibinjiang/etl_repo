a6548f05bdea9aac941dcc07a95bdd73340f487b
jiangzhibin <jiangzhibin@dailyinnovation.biz>
2020-07-10 00:24:58 +0800

    debug

--- bozz/extract_company_info.py
+++ bozz/extract_company_info.py
@@ -92,8 +92,9 @@ def extract_company():
         parse_doc = process_each_company(doc)
         model = BozzCompanyModel.dict2model(parse_doc, model)
         models.append(model)
-        save_batch(models, chunk_size=batch_size)
-        models = list()
+        if len(models) == batch_size:
+            save_batch(models, chunk_size=batch_size)
+            models = list()
 
 
 if __name__ == '__main__':
