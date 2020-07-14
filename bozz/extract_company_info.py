eb901e61d36153250ecb4bd1be0018f27d0d3bb4
jiangzhibin <jiangzhibin@dailyinnovation.biz>
2020-07-14 21:10:08 +0800

    debug

--- bozz/extract_company_info.py
+++ bozz/extract_company_info.py
@@ -116,5 +116,5 @@ if __name__ == '__main__':
     """
     python -m bozz.extract_company_info
     """
-    # extract_company()
-    merge_all_tags()
+    extract_company()
+    # merge_all_tags()
