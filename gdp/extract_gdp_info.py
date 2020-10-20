17cda82521acf6e219afddd9a51150c0dbc5f9c3
jiangzhibin <jiangzhibin@dailyinnovation.biz>
2020-10-20 10:45:53 +0800

    add country

--- gdp/extract_gdp_info.py
+++ gdp/extract_gdp_info.py
@@ -7,31 +7,52 @@ from gdp.models import CountryGDPModel
 def load_country_map():
     abbr_name_map = dict()
     for model in CountryModel.query():
-        abbr_name_map[model.iso_abbr] = model.en_name
+        # abbr_name_map[model.iso_abbr] = model.en_name
+        abbr_name_map[model.en_name] = model.iso_abbr
     return abbr_name_map
 
 
 def main():
+    mappings = {
+        'Bahamas, The': 'The Bahamas',
+        "Cote d'Ivoire": "Côte d'Ivoire",
+        'Congo, Dem. Rep.': 'Democratic Republic of the Congo',
+        'Congo, Rep.': 'Republic of the Congo',
+        'Egypt, Arab Rep.': 'Egypt',
+        'Gambia, The': 'The Gambia',
+        'Korea, Rep.': 'South Korea',
+        'Korea, Dem. People’s Rep.': 'North Korea',
+        'Hong Kong SAR, China': 'Hong Kong',
+        'Lao PDR': 'Laos',
+        'Macao SAR, China': 'Macao',
+        'West Bank and Gaza': 'Palestine',
+        'Russian Federation': 'Russia',
+        "Sao Tome and Principe": "São Tomé and Príncipe",
+        "Yemen, Rep.": 'Yemen',
+        'Iran, Islamic Rep.': 'Iran',
+        'Kyrgyz Republic': 'Kyrgyzstan',
+    }
     country_map = load_country_map()
-    df = pd.read_csv('./gdp/世界银行gdp_20200908.csv')
+    df = pd.read_csv('./gdp/世界银行GDP_20200916_en.csv')
     for i, row in df.iterrows():
-        country_code = row['Country Code']
-        if country_code not in country_map:
-            print("No country code: ", country_code)
+        country_name, country_code = row['Country Name'], row['Country Code']
+        country_name = mappings[country_name] if country_name in mappings else country_name
+        if country_name not in country_map:
+            print("No country name: ", country_name, country_code)
             continue
         for year in range(1960, 2020):
             if not row.get(str(year)) or pd.isna(row[str(year)]):
                 continue
-            if CountryGDPModel.get_by(CountryGDPModel.year == year, CountryGDPModel.country_abbr == country_code):
+            if CountryGDPModel.get_by(CountryGDPModel.year == year, CountryGDPModel.country == country_name):
                 continue
             model = CountryGDPModel()
-            model.country = country_map[country_code]
-            model.country_abbr = country_code
+            model.country = country_name
+            model.country_abbr = country_map[country_name]
             model.year = year
             model.gdp = row[str(year)]
             model.created = datetime.utcnow()
             model.updated = datetime.utcnow()
-            model.save()
+            # model.save()
 
 
 if __name__ == '__main__':
